525cf642846c77383c711c5677b95f5b4e0fc1d4
Natsugure <natsugure5301102@gmail.com>
2026-05-20 22:27:30 +0900

    stats, articlesのエンドポイント・スキーマを定義

---
+++ src/services/stats.ts
@@ -0,0 +1,20 @@
+import { createDb } from "../db/client";
+import { eq, type InferInsertModel } from "drizzle-orm";
+import { stats } from "../db/schema";
+
+export async function createStats(db: ReturnType<typeof createDb>, data: InferInsertModel<typeof stats>) {
+  try {
+    const newStats = await db.insert(stats).values({
+      articleId: data.articleId,
+      readCount: data.readCount,
+      likeCount: data.likeCount,
+      commentCount: data.commentCount,
+      fetchedAt: data.fetchedAt,
+    }).returning()
+
+    return newStats[0]
+  } catch (e) {
+    console.error(e)
+    throw e
+  }
+}
